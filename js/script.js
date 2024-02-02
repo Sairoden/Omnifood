3a8c34fde67aeaf5c2b5651725e54acc4b51ced8
Sairoden <odingandarosa@gmail.com>
2024-02-02 18:39:40 +0800

    change logo and add smooth scrolling

--- js/script.js
+++ js/script.js
@@ -15,37 +15,6 @@ btnNavEl.addEventListener("click", () => {
 });
 
 ///////////////////////////////////////////////////////////
-// Smooth scrolling animation
-const allLinks = document.querySelectorAll("a:link");
-allLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-
-      sectionEl.scrollIntoView({
-        behavior: "smooth",
-      });
-      console.log(sectionEl);
-    }
-
-    // Close mobile navigation
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
-
-///////////////////////////////////////////////////////////
 // Sticky navigation
 
 const sectionHeroEl = document.querySelector(".section-hero");
